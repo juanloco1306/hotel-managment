18fad54d13d41ffa02f06c0f137926db09a24da9
Juan Jose Jimenez <juanjosejimenez@MacBook-Pro-de-Juan.local>
2024-02-05 22:53:19 -0600

    cuarto commit

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,10 +1,16 @@
 import HeroSection from "../../components/HeroSection/HeroSection"
+import PageSearch from "../../components/PageSearch/PageSearch"
+import Gallery from "../../components/Gallery/Gallery"
+import NewsLetter from "../../components/NewsLetter/NewsLetter"
 
 const Home = async () => {
 
   return (
     <>
       <HeroSection />
+      <PageSearch />
+      <Gallery/>
+      <NewsLetter/>
     </>
   );
 };
