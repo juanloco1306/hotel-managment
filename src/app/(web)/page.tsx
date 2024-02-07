65cd26956e7df23e98990068becc716d57d32da5
unknown <juanloco1306@gmail.com>
2024-02-06 18:49:10 -0600

    quinto commit

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,14 +1,19 @@
+import FeaturedRoom from "../../components/FeaturedRoom/FeaturedRoom"
 import HeroSection from "../../components/HeroSection/HeroSection"
 import PageSearch from "../../components/PageSearch/PageSearch"
 import Gallery from "../../components/Gallery/Gallery"
 import NewsLetter from "../../components/NewsLetter/NewsLetter"
+import { getFeaturedRoom } from '../../libs/apis';
 
 const Home = async () => {
+  const featuredRoom = await getFeaturedRoom();
+
 
   return (
     <>
       <HeroSection />
       <PageSearch />
+      <FeaturedRoom featuredRoom={featuredRoom} />
       <Gallery/>
       <NewsLetter/>
     </>
